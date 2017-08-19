3932c64fc84437b4f02fd793c2b6038d2295cf8e
XLNS-XYZ <30864336+XLNS-XYZ@users.noreply.github.com>
2017-08-19 11:45:32 +0100

    Update DataStoreServiceAPI.lua

--- DataStoreServiceAPI.lua
+++ DataStoreServiceAPI.lua
@@ -1,1 +1,14 @@
--- lua for my own reference ok
+local ServerDataStore = game:GetService("DataStoreService"):GetDataStore("PlayerStats")
+
+	if PlayerSave then
+		Credits.Value = PlayerSave[1]
+		Faction.Value = PlayerSave[2]
+		Level.Value = PlayerSave[3]
+		Kills.Value = PlayerSave[4]
+		Deaths.Value = PlayerSave[5]
+		TimePlayed.Value = PlayerSave[6]
+	else
+		local ValuesToInput = {Credits.Value, Faction.Value, Level.Value, Kills.Value, Deaths.Value, TimePlayed.Value} --Make sure these are in order according to datastore
+		ServerDataStore:SetAsync(Key, ValuesToInput)
+	end
+	
